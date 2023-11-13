1ed1ef0549aa2f8fd29a65e1a9cd737adb98571c
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-11-13 18:17:14 +0200

    turns

--- Count-To-100/src/Components/UserBoard.jsx
+++ Count-To-100/src/Components/UserBoard.jsx
@@ -6,59 +6,77 @@ const UserBoard = (props) => {
   const [num, setNum] = useState(props.rndNum);
   const [steps, setSteps] = useState(0);
 
-  function checkIfWon() {
-    if (num === 100) {
-      alert(`${props.user.username} won`);
+  function checkIfWon(newNum) {
+    if (newNum === 100) {
+      alert(`${props.user.userName} won`);
     }
   }
 
   function add() {
     setNum(num + 1);
     setSteps(steps + 1);
-    checkIfWon();
+    checkIfWon(num + 1);
+    changeTurn();
   }
   function sub() {
     setNum(num - 1);
     setSteps(steps + 1);
-    checkIfWon();
+    checkIfWon(num - 1);
+    changeTurn();
   }
   function multiply() {
     setNum(num * 2);
     setSteps(steps + 1);
-    checkIfWon();
+    checkIfWon(num * 2);
+    changeTurn();
   }
   function divide() {
     setNum(Math.floor(num / 2));
     setSteps(steps + 1);
-    checkIfWon();
+    checkIfWon(Math.floor(num / 2));
+    changeTurn();
   }
+
+  function changeTurn() {
+    if (props.index === props.connectedLength - 1) {
+      props.setUserTurn(0);
+    } else {
+      props.setUserTurn(props.userTurn + 1);
+    }
+  }
+
+  const mathButtons = (
+    <>
+      <button onClick={add}>+1</button>
+      <button onClick={sub}>-1</button>
+      <button onClick={multiply}>*2</button>
+      <button onClick={divide}>/2</button>
+    </>
+  );
+
+  const mathButtonsDisabled = (
+    <>
+      <button disabled onClick={add}>
+        +1
+      </button>
+      <button disabled onClick={sub}>
+        -1
+      </button>
+      <button disabled onClick={multiply}>
+        *2
+      </button>
+      <button disabled onClick={divide}>
+        /2
+      </button>
+    </>
+  );
   return (
     <div className={styles.playerBoard}>
       <h2>{props.user.userName}</h2>
       <h3>{num}</h3>
-      {props.hasStartedGame ? (
-        <>
-          <button onClick={add}>+1</button>
-          <button onClick={sub}>-1</button>
-          <button onClick={multiply}>*2</button>
-          <button onClick={divide}>/2</button>
-        </>
-      ) : (
-        <>
-          <button disabled onClick={add}>
-            +1
-          </button>
-          <button disabled onClick={sub}>
-            -1
-          </button>
-          <button disabled onClick={multiply}>
-            *2
-          </button>
-          <button disabled onClick={divide}>
-            /2
-          </button>
-        </>
-      )}
+      {props.hasStartedGame && props.userTurn === props.index
+        ? mathButtons
+        : mathButtonsDisabled}
       <h4>steps : {steps}</h4>
       {/* <h4>score : {score}</h4> */}
     </div>
