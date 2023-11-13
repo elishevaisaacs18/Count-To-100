a7eaa3fef95476a535cece304c73e8b3afa7994d
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-11-13 16:37:12 +0200

    log in component

--- Count-To-100/src/App.jsx
+++ Count-To-100/src/App.jsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
-import Board from './Components/Board';
-import LogIn from './Components/LogIn';
+import { useState } from "react";
+import Board from "./Components/Board";
+import LogIn from "./Components/LogIn";
 
 function App() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [connected, setConnected] = useState([]);
+  console.log('CONNECTED', connected)
 
   return (
-    <div style ={{display:"flex"}}>
-    <Board />
-    <LogIn />
+    <div style={{ display: "flex" }}>
+      <Board connected={connected} />
+      <LogIn
+        userName={userName}
+        setUserName={setUserName}
+        password={password}
+        setPassword={setPassword}
+        connected={connected}
+        setConnected={setConnected}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+localStorage.setItem(
+  "efrat",
+  JSON.stringify({
+    userName: "efrat",
+    password: "elimelech",
+    scores: [1, 4, 6, 12],
+  })
+);
+localStorage.setItem(
+  "elisheva",
+  JSON.stringify({
+    userName: "elisheva",
+    password: "isaacs",
+    scores: [2, 12, 6, 8],
+  })
+);
--- Count-To-100/src/Components/LogIn.jsx
+++ Count-To-100/src/Components/LogIn.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import styles from "../LogIn.module.css";
+const LogIn = (props) => {
+  const handleUserNameChange = (event) => {
+    props.setUserName(event.target.value);
+  };
 
-const LogIn = () => {
+  const handlePasswordChange = (event) => {
+    props.setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isUserConnected(props.userName)) {
+      console.log('YOU ARE CONNECTED ALREADY. Please log out before attempting to log in again.');
+      return; // Stop further processing
+    }
+    for (const key of Object.keys(localStorage)) {
+      if (props.userName === key) {
+        const currUser = JSON.parse(localStorage.getItem(key));
+        console.log("currUser: ", currUser);
+        if (currUser.password === props.password) {
+          connectUser(currUser);
+          break;
+        }
+      } 
+      console.log("User Name:", props.userName);
+      console.log("Password:", props.password);
+    }
+  };
+
+  function isUserConnected(userName) {
+    return props.connected.some(user => user.userName === userName);
+  }
+
+  function connectUser(user) {
+    props.setConnected([
+      ...props.connected,
+      {
+        userName: user.userName,
+        scores: user.scores,
+      },
+    ]);
+  }
+
   return (
-    <div style ={{width:"30vw", backgroundColor:"blue", height:"90vh"}}>
-      
+    <div className={styles.logInContainer}>
+      <h1>Log In</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="uName">User Name</label>
+        <input
+          className={styles.inputBox}
+          type="text"
+          id="uName"
+          name="uName"
+          value={props.userName}
+          onChange={handleUserNameChange}
+        ></input>
+        <label htmlFor="pass">Password</label>
+        <input
+          className={styles.inputBox}
+          type="password"
+          id="pass"
+          name="pass"
+          value={props.password}
+          onChange={handlePasswordChange}
+        ></input>
+        <input type="submit" className={styles.logInBtn} value="Log In"></input>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LogIn
+export default LogIn;
